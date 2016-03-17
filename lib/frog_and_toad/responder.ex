8fbbde0a0526a626b9c4348ca5d31f9014f8ff84
Zach Kemp <zvkemp@gmail.com>
2016-03-17 09:47:24 -0700

    storytime

--- lib/frog_and_toad/responder.ex
+++ lib/frog_and_toad/responder.ex
@@ -28,6 +28,71 @@ defmodule FrogAndToad.Responder do
     end
   end
 
+  defp parse_command("storytime!" <> t, _, %{ "channel" => c }, %{ id: uid } = config, { bot }) do
+    Task.start(fn ->
+      [
+        [:toadbot, narrate("wakes up")],
+        [:toadbot, "Drat!"],
+        [:toadbot, "This house is a mess. I have so much work to do."],
+        [:frogbot, narrate "looks through the window"],
+        [:frogbot, "toadbot, you are right."],
+        [:frogbot, "It is a mess."],
+        [:toadbot, narrate "pulls the covers over his head"],
+        [:toadbot, "I will do it tomorrow."],
+        [:toadbot, "Today I will take life easy."],
+        [:frogbot, narrate "comes into the house"],
+        [:frogbot, "toadbot, your pants and jacket are lying on the floor."],
+        [:toadbot, "_(from under the covers)_ Tomorrow."],
+        [:frogbot, "Your kitchen sink is filled with dirty dishes."],
+        [:toadbot, "Tomorrow."],
+        [:frogbot, "There is dust on your chairs."],
+        [:toadbot, "Tomorrow."],
+        [:frogbot, "Your windows need scrubbing."],
+        [:frogbot, "Your plants need watering."],
+        [:toadbot, "_*Tomorrow!*_"],
+        [:toadbot, "_*I will do it all tomorrow!*_"],
+        [:toadbot, narrate "sits on the edge of his bed"],
+        [:toadbot, "Blah." ],
+        [:toadbot, "I feel down in the dumps."],
+        [:frogbot, "Why?"],
+        [:toadbot, "I am thinking about tomorrow." ],
+        [:toadbot, "I am thinking about all of the many things that I will have to do."],
+        [:frogbot, "Yes, tomorrow will be a very hard day for you."],
+        [:toadbot, "But frogbot, if I pick up my pants and jacket right now, then I will not have to pick them up tomorrow, will I?"],
+        [:frogbot, "No. You will not have to."],
+        [:toadbot, narrate "picks up his clothes"],
+        [:toadbot, narrate "puts them in the closet"],
+        [:toadbot, "frogbot, if I wash my dishes right now then I will not have to wash them tomorrow, will I?"],
+        [:frogbot, "No. You will not have to."],
+        [:toadbot, narrate "washes and dries his dishes."],
+        [:toadbot, narrate "puts them in the cupboard"],
+        [:toadbot, "frogbot, if I dust my chairs and scrub my windows and water my plants right now, then I will not have to do it tomorrow, will I?"],
+        [:frogbot, "No, you will not have to do any of it."],
+        [:toadbot, narrate "dusts his chairs"],
+        [:toadbot, narrate "scrubs his windows"],
+        [:toadbot, narrate "waters his plants"],
+        [:toadbot, "There. Now I feel better."],
+        [:toadbot, "I am not in the dumps anymore."],
+        [:frogbot, "Why?"],
+        [:toadbot, "Because I have done all that work." ],
+        [:toadbot, "Now I can save tomorrow for something that I really want to do."],
+        [:frogbot, "What is that?"],
+        [:toadbot, "Tomorrow, I can just take life easy."],
+        [:toadbot, narrate "goes back to bed"],
+        [:toadbot, narrate "pulls the covers over his head and falls asleep"]
+      ] |> Enum.each(fn (line) -> storyline(line, c) end)
+    end)
+  end
+
+  defp storyline([bot, str, sleep], channel) do
+    say("#{bot}", str, channel)
+    :timer.sleep(sleep)
+  end
+
+  defp storyline([bot, str], ch) do
+    storyline([bot, str, 2000], ch)
+  end
+
   defp parse_command("echo " <> t, _, %{ "channel" => c }, %{ id: uid } = config, { bot }) do
     cond do
       Regex.scan(~r/echo/, t) |> Enum.count > 3 ->
@@ -36,12 +101,16 @@ defmodule FrogAndToad.Responder do
     end
   end
 
+  defp narrate(str) do
+    "_[#{str}]_"
+  end
+
   defp parse_command("tell " <> t, name, %{ "channel" => c } = msg, %{ id: uid } = config, { bot }) do
     case String.split(t, ~r/\s/, parts: 3) do
       [other_bot, "to", command] ->
         cond do
           Process.whereis(:"#{other_bot}:supervisor") ->
-            say(bot, "_[whispers to #{other_bot}]_", c)
+            say(bot, narrate("whispers to #{other_bot}"), c)
             parse_command("#{other_bot} #{command}", other_bot, msg, config)
           true ->
             say(bot, "No! I am afraid of talking to #{other_bot}.", c)
