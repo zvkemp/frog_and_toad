5f5413370a9cbd9715620130f6bd192b32d61529
Zach Kemp <zvkemp@gmail.com>
2017-10-01 17:15:13 -0700

    example local config

---
+++ config/dev.example.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+# copy this to dev.local.exs (which is loaded in development, but ignored by git)
+config :slack,
+  use_console: true,
+  default_channel: "CHANNEL_ID",
+  bots: [
+    %{name:    "frogbot",
+      token:   "api-token-goes-here",
+      ribbit_msg: "ribbit",
+      responder: FrogAndToad.Responder,
+      keywords: %{ "cricket" => "WHERE ARE CRICKETS?" },
+      socket_client: Slack.Console.Socket,
+      api_client: Slack.Console.APIClient,
+    }
+  ]
